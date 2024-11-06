4ca53310188fb8a0229785b1d36df32f01e2b747
ElgerC <elger.creutzberg@gmail.com>
2024-11-06 20:24:12 +0100

    added shaders

---
+++ Assets/Shadders/PostProces.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProces : MonoBehaviour
+{
+    private Material _material;
+    public Shader _shader;
+    
+    void Start()
+    {
+        _material = new Material(_shader);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination,_material);
+    }
+}
